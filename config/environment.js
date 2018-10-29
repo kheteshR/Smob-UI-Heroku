f66625af12c0f2e7a4f8a0611e9fb379b74bd9b3
kheteshR <khetaram.rotangan@rapidqube.com>
2018-10-29 15:18:07 +0530

    Deployed successfully

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.GOURL='http://localhost:3008';
+    ENV.GOURL='https://smob-back.herokuapp.com/';
     ENV['g-map'] = {
       exclude: true,
       libraries: ['places', 'geometry'],
@@ -55,7 +55,7 @@ module.exports = function(environment) {
   }
 
    if (environment === 'production') {
-    ENV.GOURL='http://119.81.59.59:3008';
+    ENV.GOURL='https://smob-back.herokuapp.com/';
     ENV.locationType = 'hash';
      ENV['g-map'] = {
       exclude: true,
